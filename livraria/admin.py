6d22b9e2f7956bac7c30abb7cbc4608bdf05a59e
heloisabett <heloisabettramosifc@gmail.com>
2023-03-20 16:46:05 -0300

    atividade 6 parte 1

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,11 +1,12 @@
+
 from django.contrib import admin
 
-from .models import Categoria
+from livraria.models import Autor, Categoria, Editora, Livro
 
-from .models import Editora
-
+admin.site.register(Autor)
 admin.site.register(Categoria)
-
+admin.site.register(Editora)
+admin.site.register(Livro)
 
 
 
--- livraria/models.py
+++ livraria/models.py
@@ -13,4 +13,25 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.titulo} ({self.quantidade})"
+        
