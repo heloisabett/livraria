7db30072de57b9b321ad6ae766e788332a60f225
heloisabett <heloisabettramosifc@gmail.com>
2023-03-24 11:08:44 -0300

    parte 6-2

--- livraria/models.py
+++ livraria/models.py
@@ -32,6 +32,11 @@ class Livro(models.Model):
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
 
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+
+
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
         
