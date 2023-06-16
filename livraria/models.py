30bbe558cabfe32c63e2c13e8caa90a31950a0a3
heloisabett <heloisabettramosifc@gmail.com>
2023-06-16 11:09:03 -0300

    parte11

--- livraria/models.py
+++ livraria/models.py
@@ -37,6 +37,8 @@ class Livro(models.Model):
     )
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
+    autores = models.ManyToManyField(Autor, related_name="livros")
+
 
 
     def __str__(self):
