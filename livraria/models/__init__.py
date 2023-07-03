3d6dce91946929921a21b356d6c29486ee3932dc
heloisabett <heloisabettramosifc@gmail.com>
2023-07-03 16:45:22 -0300

    14 apaguei o model.py

---
+++ livraria/models/__init__.py
@@ -0,0 +1,4 @@
+from .autor import Autor
+from .autor import Categoria
+from .autor import Livro
+from .autor import Editora
---
+++ livraria/models/editora.py
@@ -0,0 +1,6 @@
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
